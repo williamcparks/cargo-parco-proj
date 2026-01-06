50a4ddffdabaea220985fe65a1ce7a0b6cc2968c
William Parks <williamparks@Williams-MacBook-Air.local>
2026-01-06 10:43:02 -0600

    changed src parco proj usage

--- assets/src/lib.rs
+++ assets/src/lib.rs
@@ -3,7 +3,10 @@ use proc_macro::TokenStream;
 #[proc_macro]
 pub fn proj(input: TokenStream) -> TokenStream {
     const CONTENTS: &str = include_str!("{{proj_toml_path}}");
-    match parco_proj::macros::handler(parco_proj::syn::parse_macro_input!(input), CONTENTS) {
+    match cargo_parco_proj::macros::handler(
+        cargo_parco_proj::syn::parse_macro_input!(input),
+        CONTENTS,
+    ) {
         Ok(ok) => ok,
         Err(err) => err.into_compile_error(),
     }
